e6a1739745189acf3266cb172d162341269b146b
vinicius pereira <vfreitas@tractian.com>
2025-08-31 03:02:23 -0300

    first commit base structure of the project

---
+++ lib/computorv1.ex
@@ -0,0 +1,18 @@
+defmodule Computorv1 do
+  @moduledoc """
+  Documentation for `Computorv1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Computorv1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/computorv1_test.exs
@@ -0,0 +1,8 @@
+defmodule Computorv1Test do
+  use ExUnit.Case
+  doctest Computorv1
+
+  test "greets the world" do
+    assert Computorv1.hello() == :world
+  end
+end
